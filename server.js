1a1cc4bca216af1243a890ceb8621120568eae90
Science001 <30399148+Science001@users.noreply.github.com>
2017-08-06 02:18:13 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -88,7 +88,7 @@ function createTemplate(data) {
 
 app.get('/:articleName',function (req, res){
     var articleName = req.params.articleName;
-   res.send(createTemplate(articles[articlesName])); 
+   res.send(createTemplate(articles[articleName])); 
 });
 
 app.get('/article-two', function (req, res){
