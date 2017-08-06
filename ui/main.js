a49c48ad0fd885a875e912743b1acc526f9585a6
Science001 <30399148+Science001@users.noreply.github.com>
2017-08-06 23:38:12 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -2,6 +2,11 @@ console.log('Loaded!');
 
 //Move image
 var img = document.getElementById("madi");
+var marginLeft=0;
+function moveRight(){
+    marginLeft++;
+    img.style.marginLeft = marginLeft + 'px';
+}
 img.onclick = function(){
-    img.style.marginLeft='100px';
+    var interval = setInterval(moveRight, 10);
 };
