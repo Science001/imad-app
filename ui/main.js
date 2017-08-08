2a4be5509b66f4d831c657d1c40a7070577ba458
Science001 <30399148+Science001@users.noreply.github.com>
2017-08-08 13:18:42 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -30,14 +30,27 @@ var name = nameInput.value;
 var submit = document.getElementById('submit');
 
 submit.onclick = function() {
-    //Request
-    console.log('click');
-    //Render
-    var names = ['name1','name2','name3','name4'];
-    var list ='';
-    for(var i=0; i<names.length; i++) {
-        list += '<li>' + names[i] + '</li>';
-    }
-    var ul = document.getElementById('namesList');
-    ul.innerHTML = list;
+    //create request object
+    var request = new XMLHttpRequest();
+    
+    //What to do with the request response
+    request.onreadystatechange = function() {
+        if(request.readyState === XMLHttpRequest.DONE) {
+            if(request.status === 200) {
+                //Render
+                var names = request.responseText;
+                names = JSON.parse(names);
+                var list ='';
+                for(var i=0; i<names.length; i++) {
+                    list += '<li>' + names[i] + '</li>';
+                }
+                var ul = document.getElementById('namesList');
+                ul.innerHTML = list;
+            }
+        }
+    };
+    
+    //Make the request
+    request.open('GET','http://scienceganesh007.imad.hasura-app.io/submit-name?name=' + name,true);
+    request.send(null);
 };
