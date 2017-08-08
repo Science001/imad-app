542491678b86462dd7478a61503a3f574048040a
Science001 <30399148+Science001@users.noreply.github.com>
2017-08-08 10:48:47 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -10,10 +10,15 @@ button.onclick = function() {
     //What to do with the request response
     request.onreadystatechange = function() {
         if(request.readyState === XMLHttpRequest.DONE) {
+            console.log('request.readyState === 4');
             if(request.status === 200) {
+                console.log('request.status === 200');
                 var counter = request.responseText;
+                console.log('var counter = ' + counter);
                 var span = document.getElementById('countvalue');
+                console.log('var span = ' + span);
                 span.innnerHTML = counter.toString();
+                console.log('span.innerHTML = ' + span);
             }
         }
     };
