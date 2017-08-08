fe8959fe66c2830ec373d45f0d3b73690b52f178
Science001 <30399148+Science001@users.noreply.github.com>
2017-08-08 10:36:13 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -1,6 +1,22 @@
 //Counter code
 
 var button = document.getElementById('counter');
+
 button.onclick = function() {
     
-}
+    //create request object
+    var request = new XMLHttpRequest();
+    
+    //What to do with the request response
+    request.onreadystatechange = function() {
+        if(request.readyState === XMLHttpRequest.DONE && request.status === 200) {
+            var counter = request.responseText;
+            var span = document.getElementById('count');
+            span.innnerHTML = counter;
+        }
+    };
+    
+    //Make the request
+    request.open('GET','http://scienceganesh007.imad.hasura-app.io/counter',true);
+    request.send(null);
+};
