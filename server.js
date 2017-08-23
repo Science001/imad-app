6188fe2091f59298673aca1bc43cb42c8ffbbd82
Science001 <30399148+Science001@users.noreply.github.com>
2017-08-23 21:00:10 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -142,7 +142,7 @@ app.get('/articles/:articleName',function (req, res){
     pool.query("SELECT * FROM article WHERE title = '" + req.params.articleName + "'", function(err, result) {
         if(err) {
             res.status(500).send(err.toString());
-        } else if(results.rows.length === 0) {
+        } else if(result.rows.length === 0) {
             res.status(404).send('404: Article not found');
         } else {
             var articleData = result.rows[0];
