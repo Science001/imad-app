f81a1e0e8bbb44321c4fe0734c4f71e48b1373f8
Science001 <30399148+Science001@users.noreply.github.com>
2017-08-31 21:58:21 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -51,6 +51,16 @@ function createTemplate(data) {
     return htmlTemplate;
 }
 
+function hash(input, salt) {
+    var hashed = crypto.pbkdf2Sync(input, salt, 10000, 512, 'sha512');
+    return hashed.toString('hex');
+}
+
+app.get("/hash/:input", function(req, res) {
+    var hashedString = hash(req.params.input, 'this-is-some-random-string');
+    res.send(hashedString);
+});
+
 var pool = new Pool(config);
 app.get('/test-db', function (req, res){
     pool.query('SELECT * FROM test', function (err, result) {
