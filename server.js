5b41df55ee0d1ed73a84d2b8b058632c5316607a
Science001 <30399148+Science001@users.noreply.github.com>
2017-09-02 18:31:33 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -6,14 +6,6 @@ var crypto = require('crypto');
 var bodyParser = require('body-parser');
 var session = require('express-session');
 
-var config = {
-    user: 'scienceganesh007',
-    database: 'scienceganesh007',
-    host: 'db.imad.hasura-app.io',
-    port: '5432',
-    password: process.env.DB_PASSWORD
-};
-
 var app = express();
 app.use(morgan('combined'));
 app.use(bodyParser.json());
@@ -22,11 +14,44 @@ app.use(session({
     cookie: { maxAge: 1000 * 60 * 60 * 24 * 30}
 }));
 
+
+//Resources
 app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'index.html'));
 });
+app.get('/ui/style.css', function (req, res) {
+  res.sendFile(path.join(__dirname, 'ui', 'style.css'));
+});
+app.get('/ui/main.js', function (req, res) {
+  res.sendFile(path.join(__dirname, 'ui', 'main.js'));
+});
+app.get('/ui/madi.png', function (req, res) {
+  res.sendFile(path.join(__dirname, 'ui', 'madi.png'));
+});
 
 
+//Database
+var config = {
+    user: 'scienceganesh007',
+    database: 'scienceganesh007',
+    host: 'db.imad.hasura-app.io',
+    port: '5432',
+    password: process.env.DB_PASSWORD
+};
+var pool = new Pool(config);
+app.get('/test-db', function (req, res){
+    pool.query('SELECT * FROM test', function (err, result) {
+        if(err)
+        {
+            res.status(500).send(err.toString());
+        } else {
+            res.send(JSON.stringify(result.rows));
+        }
+    });
+});
+
+
+//Articles
 function createTemplate(data) {
     var title=data.title;
     var date=data.date;
@@ -58,17 +83,33 @@ function createTemplate(data) {
     `;
     return htmlTemplate;
 }
+app.get('/articles/:articleName',function (req, res){
+    
+    pool.query("SELECT * FROM article WHERE title = $1", [req.params.articleName], function(err, result) {
+        if(err) {
+            res.status(500).send(err.toString());
+        } else if(result.rows.length === 0) {
+            res.status(404).send('404: Article not found');
+        } else {
+            var articleData = result.rows[0];
+            res.send(createTemplate(articleData));
+        }
+    });
+});
 
+
+//Password
 function hash(input, salt) {
     var hashed = crypto.pbkdf2Sync(input, salt, 10000, 512, 'sha512');
     return ["pdkdf2", "10000", salt, hashed.toString('hex')].join('$');
 }
-
 app.get("/hash/:input", function(req, res) {
     var hashedString = hash(req.params.input, 'this-is-some-random-string');
     res.send(hashedString);
 });
 
+
+//Users
 app.post('/create-user', function(req, res) {
     //Details
     var username = req.body.username;
@@ -84,7 +125,6 @@ app.post('/create-user', function(req, res) {
         }
     });
 });
-
 app.post('/login', function(req, res) {
     var username = req.body.username;
     var password = req.body.password;
@@ -117,70 +157,16 @@ app.post('/login', function(req, res) {
         }
     });
 });
-
+app.get('/logout', function(req,res) {
+   delete req.session.auth;
+   res.send('You are logged out');
+});
 app.get('/check-login', function(req,res){
    if(req.session && req.session.auth && req.session.auth.userId) {
        res.send('You are logged in - ' + req.session.auth.userId.toString());
    } else {
        res.send('You are NOT logged in');
    }
-});
-
-app.get('/logout', function(req,res) {
-   delete req.session.auth;
-   res.send('You are logged out');
-});
-
-var pool = new Pool(config);
-app.get('/test-db', function (req, res){
-    pool.query('SELECT * FROM test', function (err, result) {
-        if(err)
-        {
-            res.status(500).send(err.toString());
-        } else {
-            res.send(JSON.stringify(result.rows));
-        }
-    });
-});
-
-var counter=0;
-app.get('/counter', function(req, res) {
-   counter = counter + 1;
-   res.send(counter.toString());
-});
-
-var names =[];
-app.get('/submit-name', function(req, res) {
-    var name=req.query.name;
-    names.push(name);
-    res.send(JSON.stringify(names));
-    
-});
-
-app.get('/ui/style.css', function (req, res) {
-  res.sendFile(path.join(__dirname, 'ui', 'style.css'));
-});
-
-app.get('/ui/main.js', function (req, res) {
-  res.sendFile(path.join(__dirname, 'ui', 'main.js'));
-});
-
-app.get('/ui/madi.png', function (req, res) {
-  res.sendFile(path.join(__dirname, 'ui', 'madi.png'));
-});
-
-app.get('/articles/:articleName',function (req, res){
-    
-    pool.query("SELECT * FROM article WHERE title = $1", [req.params.articleName], function(err, result) {
-        if(err) {
-            res.status(500).send(err.toString());
-        } else if(result.rows.length === 0) {
-            res.status(404).send('404: Article not found');
-        } else {
-            var articleData = result.rows[0];
-            res.send(createTemplate(articleData));
-        }
-    });
 });
 
 
