4a91e0f78bf637d5d04586516ca99080aeb195d0
Science001 <30399148+Science001@users.noreply.github.com>
2017-09-10 13:12:06 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -121,7 +121,8 @@ app.post('/create-user', function(req, res) {
         {
             res.status(500).send(err.toString());
         } else {
-            res.send("User successfully created: " + username);
+            var sendResponse = { "message": "User successfully created: " + username };
+            res.send(JSON.stringify(sendResponse));
         }
     });
 });
