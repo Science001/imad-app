c75c89ea8a2f60d3033f2903be27685a815175cf
Science001 <30399148+Science001@users.noreply.github.com>
2017-09-10 12:01:38 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -26,9 +26,35 @@ submit.onclick = function() {
     //Make the request
     var username = document.getElementById('username').value;
     var password = document.getElementById('password').value;
-    console.log(username);
-    console.log(password);
     request.open('POST','http://scienceganesh007.imad.hasura-app.io/login', true);
+    request.setRequestHeader('Content-Type','application/json');
+    request.send(JSON.stringify({username: username, password: password}));
+};
+
+var signup = document.getElementById('submit_btn_signup');
+
+signup.onclick = function() {
+    //create request object
+    var request = new XMLHttpRequest();
+    
+    //What to do with the request response
+    request.onreadystatechange = function() {
+        if(request.readyState === XMLHttpRequest.DONE) {
+            if(request.status === 200) {
+                //Render
+                console.log("Log in success");
+                alert('Registered successfully');
+            }
+            else if (request.status === 500) {
+                alert('Something went wrong!');
+            }
+        }
+    };
+    
+    //Make the request
+    var username = document.getElementById('username').value;
+    var password = document.getElementById('password').value;
+    request.open('POST','http://scienceganesh007.imad.hasura-app.io/create-user', true);
     request.setRequestHeader('Content-Type','application/json');
     request.send(JSON.stringify({username: username, password: password}));
 };
